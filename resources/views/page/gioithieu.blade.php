1c8307cbb837735b4b844f76f2e675075de7f294
TechCare <chienduy2020@gmail.com>
2021-03-29 02:05:51 +0700

    first commit

---
+++ resources/views/page/gioithieu.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+@section('content')
+	<div class="container">	
+		<div class="space30">&nbsp;</div>
+		<h1 style="text-align: center; font-size:50px;font-weight: bold;color:green;">Giới Thiệu</h1><br>	
+		<h2 style="text-align: center;font-weight: bold;color:red;" >CÔNG TY TNHH 1 THÀNH VIÊN CD-COMPANY</h2><br>
+		<h2 style="text-align: center;font-weight: bold;color:red;">Trường Đại Học Kiến Trúc Đà Nẵng</h2><br>
+		<h3 style="text-align: center">Sinh viên : Hồ Duy Chiến</h3><br>
+		<h3 style="text-align: center">Lớp: 18CT3</h3>
+		<div class="space30">&nbsp;</div>
+
+	</div>
+
+@endsection
